18afeceaa292aed30bc249d894351aa0f3a8dfb2
Papunjay Kumar <papunjaykumar127@gmail.com>
2023-05-10 07:56:20 +0530

    Added the homeController index.html page and register.htm page

--- src/main/java/com/OVS/controller/HomeController.java
+++ src/main/java/com/OVS/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.OVS.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.OVS.model.User;
 
 @Controller
 public class HomeController {
@@ -10,4 +18,15 @@ public class HomeController {
 	public String home() {
 		return "index";
 	}
+	
+	@GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        // create model object to store form data
+       // UserDto user = new UserDto();
+		User user=new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+	
+	
 }
